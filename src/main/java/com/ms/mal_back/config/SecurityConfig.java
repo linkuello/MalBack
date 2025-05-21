214d7f7c8b884989d65c6e69349b7fad1ed98288
linkuello <144430394+linkuello@users.noreply.github.com>
2025-05-21 07:00:52 +0600

    Update SecurityConfig.java

--- src/main/java/com/ms/mal_back/config/SecurityConfig.java
+++ src/main/java/com/ms/mal_back/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3032",
                 "http://localhost:4200",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://malsat.onrender.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
